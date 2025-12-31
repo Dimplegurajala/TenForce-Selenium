04ac4328956f30d16d196d61e24eaca5b40f8293
Dimplegurajala <dimplegurajala@gmail.com>
2025-12-31 11:23:20 +0530

    ci:made changes to make sure the automation ran in headed mode in local terminal and headless in GIT pipeleine

--- conftest.py
+++ conftest.py
@@ -1,20 +1,28 @@
 import pytest
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def driver():
+
+    is_ci = os.getenv('CI', 'false').lower()=='true'
     
     # Setup Chrome Options
     options = Options()
-    options.add_argument("--headless=new") #headless for github integration
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--start-maximized") # Handle maximizing here
-    options.add_argument("--disable-gpu")
+    if is_ci:
+         options.add_argument("--headless=new") #headless for github integration
+         options.add_argument("--no-sandbox")
+         options.add_argument("--disable-dev-shm-usage")
+         options.add_argument("--start-maximized") # Handle maximizing here
+         options.add_argument("--disable-gpu")
+    else:
+         print("\n [Info] Local environment detected- Running headaed mode for review")
+         
     
     # Initialize Driver
     driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
     
     # Yield driver to the test
     yield driver
